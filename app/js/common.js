afd6be641ba61391aacef52b07c221ca726d3b6d
voland700 <voland700@yandex.ru>
2019-05-10 16:53:55 +0300

    адаптация, +footer

--- app/js/common.js
+++ app/js/common.js
@@ -52,3 +52,60 @@ document.getElementById('menuBtn').onclick = function () {
 			$(this).removeClass("animated").removeClass("fadeInUp").css("opacity", 0)
 		})
 	}
+
+$(".footer-carousel").owlCarousel({
+	items: 1,
+	loop: true,
+	nav: true,
+	navText: false,
+	autoplay: true,
+	smartSpeed: 1000,
+	autoplayTimeout: 5000,
+	autoplayHoverPause: true,
+	addClassActive: true,
+	animateOut: 'fadeOut',
+	baseClass: 'footer-carousel',
+	stageClass: 'footer-stage',
+	stageOuterClass: 'footer-stage-outer',
+	navText: ['&lsaquo;', '&rsaquo;'],
+	itemClass: 'fcar-item',
+	navContainerClass: 'fcar-nav',
+	navClass: ['fcar-prev', 'fcar-next'],
+	controlsClass: 'fcar-controls',
+	dotClass: 'fcar-dot',
+	dotsClass: 'fcar-dots'
+});
+
+// Кнопка вверх
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 500) {
+		if ($('#upbutton').is(':hidden')) {
+			$('#upbutton').css({
+				opacity: .3
+			}).fadeIn('slow');
+		}
+	} else {
+		$('#upbutton').stop(true, false).fadeOut('fast');
+	}
+});
+$('#upbutton').click(function () {
+	$('html, body').stop().animate({
+		scrollTop: 0
+	}, 300);
+});
+
+$('#upbutton').hover(function () {
+	$('#upbutton').css({
+		opacity: .8
+	});
+}, function () {
+	$('#upbutton').css({
+		opacity: .3
+	});
+});
+
+$(".goods_img-link").click(function () {
+	let img = $(this).attr("href");
+	$("#GoodsImg").attr("src", img);
+	return false;
+});
